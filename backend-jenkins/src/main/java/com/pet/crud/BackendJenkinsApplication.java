81739729c5f6fee07ffc3cb19529cfb4e65c74fe
SHAIK-YAKUB <2300030833@kluniversity.in>
2025-09-23 17:17:47 +0530

    updated

--- backend-jenkins/src/main/java/com/pet/crud/BackendJenkinsApplication.java
+++ backend-jenkins/src/main/java/com/pet/crud/BackendJenkinsApplication.java
@@ -2,15 +2,20 @@ package com.pet.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendJenkinsApplication extends SpringBootServletInitializer{
+public class BackendJenkinsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendJenkinsApplication.class, args);
-		System.out.println("Project is Running ...");
-		
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // Point to your main application class
+        return builder.sources(BackendJenkinsApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJenkinsApplication.class, args);
+        System.out.println("✅ Project is Running on External Tomcat ...");
+    }
 }
